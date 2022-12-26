1df96d384822d3a010d073994d950c7b60c5ec97
alpha <alphaomar.diallo@gmail.com>
2022-12-26 12:36:12 +0200

    section 4 over

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Alpha");
+
+        boolean isAlien = false;
+
+        if (!isAlien) {
+            System.out.println("is is not an alien!");
+            System.out.println("I am scared of aliens!");
+        }
+
+        int topScore = 100;
+
+        if (topScore != 100) {
+            System.out.println("You got the high score!");
+        }
+    }
+}
